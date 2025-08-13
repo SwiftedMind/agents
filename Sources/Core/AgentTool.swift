0e9740a61ee026aa5d349ba55de1a814b6caed86
Dennis Müller <d3mueller@me.com>
2025-08-13 18:49:32 +0200

    Add Encodable support to AgentTool protocol

--- Sources/Core/AgentTool.swift
+++ Sources/Core/AgentTool.swift
@@ -3,4 +3,19 @@
 import Foundation
 import FoundationModels
 
-public protocol AgentTool: FoundationModels.Tool where Output: ConvertibleToGeneratedContent {}
+public protocol AgentTool: FoundationModels.Tool, Encodable where Output: ConvertibleToGeneratedContent {}
+
+private enum AgentToolCodingKeys: String, CodingKey {
+  case name
+  case description
+  case parameters
+}
+
+public extension AgentTool {
+  func encode(to encoder: any Encoder) throws {
+    var container = encoder.container(keyedBy: AgentToolCodingKeys.self)
+    try container.encode(name, forKey: .name)
+    try container.encode(description, forKey: .description)
+    try container.encode(parameters, forKey: .parameters)
+  }
+}
